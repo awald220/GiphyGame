2124287d8792055b3850fa964cc1eccf13c665cb
Ashley Waldner <ashleywaldner22@gmail.com>
2020-03-26 12:43:46 -0500

    add search bar it doesn't work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 var topics = ["beaver", "cat", "bird", "koala", "panda", "rat", "rooster", "skunk", "otter", "pig", "flamingo", "yak", "shark", "rabbit", "cow"]
 
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=3UrbvjjXIdCJBLf193bSUFx6flMcvxUC";
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=3UrbvjjXIdCJBLf193bSUFx6flMcvxUC&rating=&limit=&q=";
 
 $.ajax({
     url: queryURL,
@@ -8,14 +8,30 @@ $.ajax({
 }).then(function(response){
     console.log(response)
 
+    // This loops throught the topic array and makes a button for each index
     for (var i = 0; i < topics.length; i++) {
         var addButton = $("<button>")
         $("#buttons").append(addButton)
         addButton.text(topics[i]);
     }
 
-  
+    // Gives buttons an on click event
+    $("#buttons").on("click", function(){
+        console.log("work dang it!!!")
+    })
    
+    function addNewButton() {
+        $("#mySearch").on("click", function() {
+            var animal = $("#topicInput").val().trim();
+            if (animal == ""){
+                return false;//no blank buttons
+            }
+            topic.push(animal);
+    
+            displayGifButtons();
+            return false;
+            });
+    }
 
 });
 
